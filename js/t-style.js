ca4c9fc474262c3767869380e91f14ed1c346452
SaSaChu <teresa7397@gmail.com>
2017-03-22 12:59:47 +0800

    Fix html code.

--- js/t-style.js
+++ js/t-style.js
@@ -1,7 +1,4 @@
-/**
- * @author      ZEUS Design - http://www.zeusdesign.com.tw
- * @copyright   Copyright (c) 2016 ZEUS Design
- */
+
 
 $(function () {
   $('#pages a').click (function (argument) {
