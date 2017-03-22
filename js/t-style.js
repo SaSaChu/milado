88402b00452fcfca79cd60b37808b89dcccd99ce
OA Wu <comdan66@gmail.com>
2017-03-22 12:14:33 +0800

    Fix code.

--- js/t-style.js
+++ js/t-style.js
@@ -5,6 +5,9 @@
 
 $(function () {
   $('#pages a').click (function (argument) {
-    $('#money-viedos').attr ('class', 'content-wrap money n' + ($(this).index () + 1));
+    $('#forgo3').attr ('class', 'content-wrap money n' + ($(this).index () + 1));
+  });
+  $('a[data-forgo]').click (function () {
+    $('body').stop ().animate ({ scrollTop: $('#' + $(this).data ('forgo')).offset ().top - (60) }, 1000);
   });
 });
