baa4c4b88f3c4e0a0e9e2292a4d51fa67b89de85
SaSaChu <teresa7397@gmail.com>
2017-03-23 09:27:56 +0800

    詳細頁修正＋我的募資案開版

--- js/t-style.js
+++ js/t-style.js
@@ -6,5 +6,27 @@ $(function () {
   });
   $('a[data-forgo]').click (function () {
     $('body').stop ().animate ({ scrollTop: $('#' + $(this).data ('forgo')).offset ().top - (60) }, 1000);
+    $(this).addClass ('active').parent ().siblings ().find ('a').removeClass ('active');
+
   });
+
+  var tags_top = $('#tags').offset ().top;
+  
+  $(window).scroll (function () {
+    if (($(window).scrollTop () + 150) > tags_top) {
+      $('#tags').addClass ('fix');
+    } else {
+      $('#tags').removeClass ('fix');
+    }
+    if ($('#ad').data ('show')) {
+      if ($(window).scrollTop () > $('#ad').data ('show')) {
+        $('#ad').addClass ('show');
+      } else {
+        $('#ad').removeClass ('show');
+      }
+    }
+  });
+  // $('#viedos .viedo .obj').click (function () {
+    
+  // });
 });
