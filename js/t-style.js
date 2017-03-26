3624addd8c4e4611604389967c0ef74c06d92680
SaSaChu <teresa7397@gmail.com>
2017-03-26 21:34:35 +0800

    如何運作，剩下成功範例

--- js/t-style.js
+++ js/t-style.js
@@ -11,7 +11,6 @@ $(function () {
   $('a[data-forgo]').click (function () {
     $('body').stop ().animate ({ scrollTop: $('#' + $(this).data ('forgo')).offset ().top - (60) }, 1000);
     $(this).addClass ('active').parent ().siblings ().find ('a').removeClass ('active');
-
   });
 
   var tags_top = $('#tags').offset ().top;
@@ -30,7 +29,31 @@ $(function () {
       }
     }
   });
-  // $('#viedos .viedo .obj').click (function () {
+
+});
+
+
+
+
+$(function () {
+  var menu_top = $('.navboxs').offset ().top;
+  $(window).scroll (function () {
     
-  // });
+    if ($(window).scrollTop () > menu_top) {
+      $('.navboxs').addClass ('fix2');
+    } else {
+      $('.navboxs').removeClass ('fix2');
+    }
+
+    if (!$('#icons').hasClass ('show') && ($(window).scrollTop () + $(window).height ()) > $('#icons').offset ().top) {
+      
+    console.error ('x');
+      $('#icons').addClass ('show');
+    }
+    if (!$('#box').hasClass ('show') && ($(window).scrollTop () + $(window).height ()) > $('#box').offset ().top) {
+      $('#box').addClass ('show');
+    }
+    
+  });
+
 });
